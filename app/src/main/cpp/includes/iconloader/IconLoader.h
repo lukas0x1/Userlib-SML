e23638da99162ffa3f982e161041fda8f67d9997
ArtDev <45949002+artdeell@users.noreply.github.com>
2023-04-06 08:14:15 +0300

    Update IconLoader.h

--- app/src/main/cpp/includes/iconloader/IconLoader.h
+++ app/src/main/cpp/includes/iconloader/IconLoader.h
@@ -10,6 +10,13 @@ public:
     ImVec2 size;
 };
 
+class UIIcon {
+public:
+    ImTextureID textureId;
+    ImVec2 uv0;
+    ImVec2 uv1;
+};
+
 class IconLoader {
 public:
     /**
@@ -26,6 +33,12 @@ public:
     * Creates a button with an icon, loaded by its name from UiPackedAtlas.lua, with the specified name and size in pixels
     */
     static bool iconButton(const std::string& name, const float& size, const ImVec4& color = ImVec4(1,1,1,1));
+    /**
+    * Gets the required parameters for an icon, loaded by its name from UiPackedAtlas.lua, with the specified name
+    * If icon->textureId is IL_NO_TEXTURE, the icon data is not valid and it shouldn't be drawn.
+    * Note: you should not store the result of this function outside of your draw function's scope
+    */
+    static void getUIIcon(const std::string &name, UIIcon* icon);
 };
 
 
